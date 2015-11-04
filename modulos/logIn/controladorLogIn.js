461cf99e7857ca521df0fc01afde42cb0632b036
amayor <africamc.94@gmail.com>
2015-11-04 16:26:34 +0100

    controlador del loggin con control de campos vacíos con toasts

--- modulos/logIn/controladorLogIn.js
+++ modulos/logIn/controladorLogIn.js
@@ -1,8 +1,8 @@
-app.controller('controladorLogIn', function(servicioRest, config, $scope, $location, $rootScope) {
+app.controller('controladorLogIn', function(servicioRest, config, $scope, $location, $rootScope, $mdToast) {
 
 
 	$scope.login = function () {
-        $rootScope.cargando=true;
+        $rootScope.cargando = true;
 		var user = {};
 		user.username = $scope.user;
 		user.password = $scope.pass;
@@ -16,8 +16,39 @@ app.controller('controladorLogIn', function(servicioRest, config, $scope, $locat
 				}
 			})
 			.catch(function(err) {
-                $rootScope.cargando=false;
+                $rootScope.cargando = false;
 				console.log("Error");
+				if($scope.user == null) {
+					if($scope.pass == null) {
+						$mdToast.show(
+					      $mdToast.simple()
+					        .content('Debe introducir su usuario y contraseña.')
+					        .position('top right')
+					        .hideDelay(1500)
+					    );
+					} else {
+						$mdToast.show(
+					      $mdToast.simple()
+					        .content('Debe introducir su usuario.')
+					        .position('top right')
+					        .hideDelay(1500)
+					    );
+					}
+				} else if($scope.pass == null) {
+					$mdToast.show(
+				      $mdToast.simple()
+				        .content('Debe introducir su contraseña.')
+				        .position('top right')
+				        .hideDelay(1500)
+				    );
+				} else {
+					$mdToast.show(
+				      $mdToast.simple()
+				        .content('El usuario o la contraseña es incorrecta.')
+				        .position('top right')
+				        .hideDelay(1500)
+				    );
+				}
 			});
 	}
 
