7f85266acf9f8a106e3e08adaadf71093e1fb0d9
AlmudenaBarreroMariscal <almubarrero@hotmail.com>
2015-11-12 10:22:27 +0100

    icono crear para poder colocarlo

--- modulos/tech/controladorTech.js
+++ modulos/tech/controladorTech.js
@@ -1,22 +1,22 @@
-app.controller('controladorTech', function(servicioRest, config, $scope, $location, $rootScope, $mdDialog) {
+app.controller('controladorTech', function(servicioRest, config,$scope, $location, $rootScope, $mdDialog) {
     
     /*---------------------------Inicializar vista------------------------------*/
     
-	$rootScope.cargando=false;
+	$rootScope.cargando = false;
     
     $scope.preguntas = [
-      {
-        titulo: 'hola this weekend?',
-        tema: 'Min Li Chan',
-        nivel: '3:08PM',
-        tipo: " I'll be in your neighborhood doing errands"
-      },
-      {
-        titulo: 'hola this weekend?',
-        tema: 'Min Li Chan',
-        nivel: '3:08PM',
-        tipo: " I'll be in your neighborhood doing errands"
-      },
+        {
+            titulo: 'hola this weekend?',
+            tema: 'Min Li Chan',
+            nivel: '3:08PM',
+            tipo: " I'll be in your neighborhood doing errands"
+        },
+        {
+            titulo: 'hola this weekend?',
+            tema: 'Min Li Chan',
+            nivel: '3:08PM',
+            tipo: " I'll be in your neighborhood doing errands"
+        }
     ];
     
     /*--------------------------Funciones--------------------------*/
@@ -28,42 +28,7 @@ app.controller('controladorTech', function(servicioRest, config, $scope, $locati
             nivel: '6',
             tipo: "test"
         })
-    }
-    
-    $scope.modificar = function () {
-        /*function(item) {
-        angular.element(document.getElementById("modal")).scope().item = item;
-    };*/
-        /*$rootScope.cargando = true;
-		var user = {};
-		user.username = $scope.user;
-		user.password = $scope.pass;
-		servicioRest.postAuthenticate(user)
-			.then(function(data) {
-				console.log(data.role);
-				if(data.role === "ROLE_ADMIN") {
-					$location.path("/admin");
-				} else {
-					$location.path("/tech");
-				}
-			})
-			.catch(function(err) {
-                $rootScope.cargando = false;
-				console.log("Error");
-				console.log(err);
-				if (err === "Servicio no disponible") {
-					toast("Error de conexión");
-				} else if($scope.user == null && $scope.pass == null) {
-					toast("Debe introducir su usuario y contraseña");
-				} else if($scope.user == null) {
-					toast("Debe introducir su usuario");
-				} else if($scope.pass == null) {
-					toast("Debe introducir su contraseña");
-				} else {
-					toast("El usuario o la contraseña es incorrecta");
-				}
-			});*/
-	}
+    };
     
     $scope.eliminar = function (indice) {
         $scope.preguntas.splice(indice,1);
@@ -80,14 +45,26 @@ app.controller('controladorTech', function(servicioRest, config, $scope, $locati
         
         */
 	}
-  
-  $scope.showTabDialog = function(ev) {
-    $mdDialog.show({
-      controller: 'controladorCrear',
-      templateUrl: 'modulos/tech/crear.tmpl.html',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      clickOutsideToClose:true
-    })
-  };
+    
+    $scope.showTabDialog = function(ev) {
+        $mdDialog.show({
+            controller: 'controladorCrear',
+            templateUrl: 'modulos/tech/crear.tmpl.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true
+        })
+    };
+    
+    function controladorCrear(servicioRest, config, $scope, $location, $rootScope, $mdDialog) {
+        $scope.hide = function(){
+            $mdDialog.hide();
+        };
+        $scope.cancel = function(){
+            $mdDialog.cancel();
+        };
+        $scope.answer = function(answer){
+            $mdDialog.hide(answer);
+        };
+    };
 });
