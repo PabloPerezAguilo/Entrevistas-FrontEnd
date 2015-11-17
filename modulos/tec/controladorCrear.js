5ca25139aa23c9785e9bd5b66f81e7386cd26f91
AlmudenaBarreroMariscal <almubarrero@hotmail.com>
2015-11-17 10:08:33 +0100

    controlador crear con menos problemas JSLint

--- modulos/tec/controladorCrear.js
+++ modulos/tec/controladorCrear.js
@@ -1,57 +1,38 @@
-app.controller('controladorCrear', function(servicioRest, config, $scope, $location, $rootScope, $mdDialog) {
+app.controller('controladorCrear', function (servicioRest, config, $scope, $location, $rootScope, $mdDialog) {
     
-    var Options={
+    var Options = {
         title: String,
         Valid: Boolean
-    }
+    };
     
-    var pregunta={
-        _id:{
-            type: String
-        },
-        title:{
-            type:String,
-            required:true 
-        },
-        type:{
-            type:String,
-            required:true
-        },
-        tags:[String],
-        level:{
-            type: Number,
-        min:1,
-        max:10,
-        required:true
-        },
-        directive:{
-            type:String
-        },
-        answers:[Options]
-    }
-    $scope.crearPAbierta=function(){
-        console.log($scope.tituloAbierta);
-        //pregunta._id=null;
-        pregunta.title=$scope.tituloAbierta;
-        pregunta.type="FREE";
-        pregunta.tags[0]="algo";
-        pregunta.level=$scope.nivelAbierta;
+    var pregunta = {
+        _id: { type: String },
+        title: { type: String, required: true },
+        type: { type: String, required: true },
+        tags: [String],
+        level: { type: Number, min: 1, max: 10, required: true },
+        directive: { type: String },
+        answers: [Options]
+    };
+	
+    $scope.crearPAbierta = function () {
+        pregunta.title = $scope.tituloAbierta;
+        pregunta.type = "FREE";
+        pregunta.tags[0] = "algo";
+        pregunta.level = $scope.nivelAbierta;
         $scope.hide(pregunta);
     };
     
-    $scope.crearPTest=function(){
-        console.log();
+    $scope.crearPTest = function () {
         $scope.hide();
     };
     
-    $scope.crearPTestAbierta=function(){
-        console.log();
+    $scope.crearPTestAbierta = function () {
         $scope.hide();
     };
     
     $scope.hide = function (respuesta) {
         $mdDialog.hide(respuesta);
-        console.log("aqui");
     };
     $scope.answer = function (answer) {
         $mdDialog.hide(answer);
@@ -61,7 +42,7 @@ app.controller('controladorCrear', function(servicioRest, config, $scope, $locat
         $mdDialog.cancel();
     };
     
-    $scope.nivel = 5;
+    $scope.nivelAbierta = $scope.nivelTest = $scope.nivelTestAbierto = 5;
 	$scope.respuestasTest = [];
 	$scope.test = ['1', '2'];
 	$scope.contTest = 2;
@@ -75,7 +56,6 @@ app.controller('controladorCrear', function(servicioRest, config, $scope, $locat
 	$scope.aniadirRespuestaTest = function () {
 		$scope.contTest += 1;
 		$scope.test.push($scope.contTest);
-		$scope.radioTest($scope.contTest);
 	};
 	
 	$scope.aniadirRespuestaTestAbierto = function () {
@@ -83,5 +63,4 @@ app.controller('controladorCrear', function(servicioRest, config, $scope, $locat
 		$scope.testAbierto.push($scope.contTestAbierto);
 		$scope.checkTestAbierto.push(false);
 	};
-
 });
