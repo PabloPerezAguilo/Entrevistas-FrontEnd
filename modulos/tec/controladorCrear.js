45a816d71cf08add5e84dc657decb3030a675483
aGonzalez94 <alejandro.powerfull@gmail.com>
2015-11-17 12:30:30 +0100

    Comprobacion de campos vacios en crear pregunta

--- modulos/tec/controladorCrear.js
+++ modulos/tec/controladorCrear.js
@@ -1,4 +1,4 @@
-app.controller('controladorCrear', function (servicioRest, config, $scope, $location, $rootScope, $mdDialog) {
+app.controller('controladorCrear', function (servicioRest, config, $scope, $location, $rootScope, $mdDialog, $mdToast) {
     
     var Options = {
         title: { type: String },
@@ -15,45 +15,102 @@ app.controller('controladorCrear', function (servicioRest, config, $scope, $loca
         answers: [Options]
     };
 	
+    function toast(texto) {
+		$mdToast.show(
+	      $mdToast.simple()
+	        .content(texto)
+	        .position('top right')
+	        .hideDelay(1500)
+		);
+	}
+    
     $scope.crearPAbierta = function () {
-        pregunta.title = $scope.tituloAbierta;
-        pregunta.type = "FREE";
-        pregunta.tags[0] = $scope.temasAbierta;
-        pregunta.level = $scope.nivelAbierta;
-        pregunta.directive = $scope.directivasAbierta;
-        $scope.hide(pregunta);
+        if(preguntaVacia("abierta"))
+        {
+            toast("Rellena todos los campos obligatorios");
+        }
+        else
+        {
+            pregunta.title = $scope.tituloAbierta;
+            pregunta.type = "FREE";
+            pregunta.tags[0] = $scope.temasAbierta;
+            pregunta.level = $scope.nivelAbierta;
+            pregunta.directive = $scope.directivasAbierta;
+            $scope.hide(pregunta);
+        }
     };
     
     $scope.crearPTest = function () {
 		var i;
-        pregunta.title = $scope.tituloTest;
-        pregunta.type = "SINGLE_CHOICE";
-        pregunta.tags[0] = $scope.temasTest;
-        pregunta.level = $scope.nivelTest;
-        pregunta.answers = [];
-
-       	for (i = 0;i < $scope.contTest;i++) {            
-            pregunta.answers.push({title: $scope.respuestasTest[i], valid: false});
+        if(preguntaVacia("test"))
+        {
+            toast("Rellena todos los campos obligatorios");
         }
-        
-        pregunta.answers[$scope.radioTest - 1].valid = true;
-        $scope.hide(pregunta);
+        else
+        {
+            pregunta.title = $scope.tituloTest;
+            pregunta.type = "SINGLE_CHOICE";
+            pregunta.tags[0] = $scope.temasTest;
+            pregunta.level = $scope.nivelTest;
+            pregunta.answers = [];
+
+            for (i = 0;i < $scope.contTest;i++) {            
+                pregunta.answers.push({title: $scope.respuestasTest[i], valid: false});
+            }
+            console.log(pregunta.title);
+
+            pregunta.answers[$scope.radioTest - 1].valid = true;
+            $scope.hide(pregunta);
+        }
     };
     
     $scope.crearPTestAbierta = function () {
 		var i;
-        pregunta.title = $scope.tituloTestAbierto;
-        pregunta.type = "MULTI_CHOICE";
-        pregunta.tags[0] = $scope.temasTestAbierto;
-        pregunta.level = $scope.nivelTestAbierto;
-        pregunta.answers = [];
-		
-		for (i = 0; i < $scope.contTestAbierto; i++) {            
-            pregunta.answers.push({title: $scope.respuestasTestAbierto[i], valid: $scope.checkTestAbierto[i]});
+        if(preguntaVacia("testAbierta"))
+        {
+            toast("Rellena todos los campos obligatorios");
         }
-        
-        $scope.hide(pregunta);
+        else
+        {
+            pregunta.title = $scope.tituloTestAbierto;
+            pregunta.type = "MULTI_CHOICE";
+            pregunta.tags[0] = $scope.temasTestAbierto;
+            pregunta.level = $scope.nivelTestAbierto;
+            pregunta.answers = [];
+
+            for (i = 0; i < $scope.contTestAbierto; i++) {            
+                pregunta.answers.push({title: $scope.respuestasTestAbierto[i], valid: $scope.checkTestAbierto[i]});
+            }
+
+            $scope.hide(pregunta);
+        }
     };
+    
+    function preguntaVacia(tipoP){
+       var resp=false;
+        if(tipoP==="abierta")
+        {
+            if($scope.tituloAbierto===undefined||$scope.temasAbierto===undefined||$scope.respuestasAbierto[0]===undefined||$scope.respuestasAbierto[1]===undefined||$scope.tituloAbierto===""||$scope.temasAbierto===""||$scope.respuestasAbierto[0]===""||$scope.respuestasAbierto[1]==="")
+            {
+                resp=true;
+            }
+        }
+        else if(tipoP==="test")
+        {
+            if($scope.tituloTest===undefined||$scope.temasTest===undefined||$scope.respuestasTest[0]===undefined||$scope.respuestasTest[1]===undefined||$scope.tituloTest===""||$scope.temasTest===""||$scope.respuestasTest[0]===""||$scope.respuestasTest[1]==="")
+            {
+                resp=true;
+            }
+        }
+        else if(tipoP==="testAbierta")
+        {
+            if($scope.tituloTestAbierto===undefined||$scope.temasTestAbierto===undefined||$scope.respuestasTestAbierto[0]===undefined||$scope.respuestasTestAbierto[1]===undefined||$scope.tituloTestAbierto===""||$scope.temasTestAbierto===""||$scope.respuestasTestAbierto[0]===""||$scope.respuestasTestAbierto[1]==="")
+            {
+                resp=true;
+            }
+        }
+        return resp;
+    }
     
     $scope.hide = function (respuesta) {
         $mdDialog.hide(respuesta);
