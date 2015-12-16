d0c206521ed75b66345c93392aa723c0801bff5d
AlmudenaBarreroMariscal <almubarrero@hotmail.com>
2015-12-16 13:07:09 +0100

    limpieza de codigo en controladorCrear.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 app.constant('config', {
 	//habra que cambiar la url
     
-	url: "http://localhost:9600/api"
+	//url: "http://localhost:9600/api"
 	//url: "http://194.140.6.149:9600/api"//,//ENRIQUE
-	//url: "http://194.140.6.59:9600/api"//, //FRAN
+	url: "http://194.140.6.59:9600/api"//, //FRAN
 	//url: "http://194.140.6.91:9600/api"//, //ALEX
 });
