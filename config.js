76aac38cccf9bb8de51bf723029514afed50d063
AlmudenaBarreroMariscal <almubarrero@hotmail.com>
2015-12-22 12:39:07 +0100

    pequeños arreglos

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ app.constant('config', {
     
 	//url: "http://localhost:9600/api"
 	//url: "http://194.140.6.149:9600/api"//,//ENRIQUE
-	url: "http://194.140.6.59:9600/api"//, //FRAN
+	//url: "http://194.140.6.59:9600/api"//, //FRAN
 	//url: "http://194.140.6.91:9600/api"//, //ALEX
-	//url: "http://194.140.6.57:9600/api"//, //AFRI
+	url: "http://194.140.6.57:9600/api"//, //AFRI
 });
