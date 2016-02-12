25de27c03dd6ac46b444c29bbfb0a35e4cbb7a9a
AlmudenaBarreroMariscal <almubarrero@hotmail.com>
2016-02-12 10:40:22 +0100

    antes cambiar arquitectura

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,46 @@
-/*
-* Dependencias
-*/
 var gulp = require('gulp'),
-concat = require('gulp-concat'),
-uglify = require('gulp-uglify'),
-connect = require('gulp-connect'),
-jshint = require('gulp-jshint'),
-war = require('gulp-war'),
-zip = require('gulp-zip'),
-open = require('gulp-open'),
-htmlreplace = require('gulp-html-replace'),
-clean = require('gulp-clean'),
-ngAnnotate = require('gulp-ng-annotate'),
-htmlify = require('gulp-angular-htmlify');
-var runSequence = require('run-sequence').use(gulp);
+lr = require('gulp-livereload');
+//sass = require('gulp-sass'),
+//jshint = require('gulp-jshint');
 
 var pathHTML=['./modulos/**/*.html','./index.html'];
 
 
-gulp.task('default', ['info']);
+var paths = {
+	app : "app",
+	lib : "app/lib",
+	scss : "css",
+	css : "css",
+	target : "target"
+};
 
-gulp.task('server', function() {
-	console.log("Arrancamos el server");
-	connect.server({
-		port: 80,
-		root: './',
-		livereload: true,
-	});
-});
+var names = {
+	anyFile : "/**/*",
+	anyJS : "/**/*.js",
+	anyHTML : "/**/*.html",
+	anyCSS : "/**/*.css",
+	anySCSS : "/**/*.scss",
+	minJS : "app.min.js",
+	minCSS : "app.min.css"
+}
 
-gulp.task('abrirTab', function(){
-	console.log("Abrimos una pestaña del navegador");
-	var options = {
-		url: 'http://localhost:80',
-		app: 'chrome'
-	};
-	gulp.src('./index.html')
-	.pipe(open('', options));
-});
 
-// minificado de todos los JS
-gulp.task('minifyJS', function () {
-	console.log("Iniciamos la unificacion y minificado de los JS");
-	gulp.src(['target/files/app.js','target/files/services.js','target/files/config.js','target/files/modulos/**/*.js'])
-	.pipe(concat('app.min.js'))
-	.pipe(ngAnnotate())
-	.pipe(uglify())
-	.pipe(gulp.dest('target/files/'));
-	return gulp.src('target/files/index.html')
-	.pipe(htmlreplace({
-		'JS': ['app.min.js']
-	},{'keepUnassigned':true,'keepBlockTags':true}))
-	.pipe(gulp.dest('target/files/'));
-});
+function startExpress() {
+	var express = require('express');
+	var app = express();
 
-gulp.task('validate', function () {
-	console.log("Iniciamos jshint");
-	return gulp.src(['**/*.js',"!node_modules/**","!node_modules","!test/**","!test","!target/**","!package.json","!gulpfile.js","!js/*"])
-	.pipe(jshint())
-	.pipe(jshint.reporter('default'))
-});
+	app.use(require('connect-livereload')());
+	app.use(express.static(__dirname + "/"));
 
-gulp.task('package',function () {
-	console.log("Iniciamos la generacion del WAR");
-	return gulp.src(["target/files/**"])
-	.pipe(war({
-		welcome: 'index.html',
-		displayName: 'Grunt WAR',
-	}))
-	.pipe(zip('LisaFront.war'))
-	.pipe(gulp.dest("target"));
+		// Start livereload
+	lr.listen(35729);
+	
+	app.listen(80);
+}
 
-});
-
-gulp.task('copy', function () {
-	console.log("Iniciamos la copia de ficheros");
-	return gulp.src(["**","!node_modules/**","!node_modules","!test/**","!test","!target/**","!target","!package.json","!gulpfile.js","!*.bat"])
-	.pipe(gulp.dest("target/files/"));
-});
-
-gulp.task('clean', function () {
-	console.log("Borramos directorio");
-	return gulp.src('target', {read: false})
-	.pipe(clean());
-});
+function notifyLivereload(file) {
+	lr.reload(file);
+}
 
 gulp.task('cdnReplace', function() {
 	console.log("Reemplazamos las librerias por los CDNs de index");
@@ -115,34 +72,11 @@ gulp.task('cdnReplace', function() {
 	.pipe(gulp.dest('target/files/'));
 });
 
-gulp.task('htmlify', function() {
-    gulp.src(['**/*.html','index.html',"!node_modules/**","!node_modules", "!target/**","!target"])
-        .pipe(htmlify())
-        .pipe(gulp.dest('target/files/'));
-});
 
-gulp.task('DEV', function () {
-	console.log("Empaquetamos modo DEV");
-	runSequence('clean','copy','package');
-});
-
-gulp.task('PRO', function () {
-	console.log("Empaquetamos modo PRO");
-	runSequence('clean','copy','minifyJS','htmlify','cdnReplace');
-});
-
-
-gulp.task('info', function () {
-	console.log("\n\n\nLISTADO DE OPCIONES");
-	console.log("------------------------");
-	console.log("'package': Solo Empaqueta el WAR");
-	console.log("'DEV': Limpia y Empaqueta el WAR");
-	console.log("'PRO': Limpia y Empaqueta el WAR minificando, poniendo los CDNs y estandarizando HTML");
-	console.log("'minifyJS': Minifica");
-	console.log("'cdnReplace': Reemplaza por los CDNs");
-	console.log("'validate': Valida los JS con jshint");
-	console.log("'start': Arranca servidor y abre pestaña navegador");
-	console.log("'server': Arranca servidor");
-	console.log("'abrirTab': Abre pestaña navegador");
-	console.log("\n\n\n");
+gulp.task('default', function () {
+	startExpress();
+	//gulp.watch(paths.scss+names.anyFile, ["compileCSS"]);
+	gulp.watch("./**/*.css", notifyLivereload);
+	gulp.watch("./**/*.js", notifyLivereload);
+	gulp.watch("./**/*.html", notifyLivereload);
 });
